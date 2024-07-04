3a538ef7b15581a44ebe6abaa72eec55f4ffe661
Hooman Mirghasemi <hooman.programmer@gmail.com>
2024-07-04 13:19:16 +0330

    init project

---
+++ gallery/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length = 50, verbose_name='نام')
+    parent_id = models.ForeignKey("self", on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length = 50, verbose_name='عنوان')
+    description = models.TextField(verbose_name='توضیحات')
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
